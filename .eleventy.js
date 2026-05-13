d332e9b79c536e775519ebbbd75eb2e6b9f4318f
Daniel Ragsdale <DanJeffRags@gmail.com>
2026-05-13 15:41:07 -0600

    Added basic navigation.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,48 @@
-module.exports = function(eleventyConfig) {
+import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
+
+export default function(eleventyConfig) {
+	eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+	eleventyConfig.addFilter('navigationPrevious', function (nav) {
+		const flat = [];
+
+		const visit = (items) => {
+			for (const item of items) {
+				if (item.url === this.page.url){
+					return flat.pop();
+				}
+				flat.push(item);
+				
+				const childReturn = visit(item.children);
+				if (childReturn) {
+					return childReturn;
+				}
+			}
+		}
+		return visit(nav);
+	});
+
+	eleventyConfig.addFilter('navigationNext', function (nav) {
+		const flat = []
+
+		const visit = (items) => {
+			for (const item of items) {
+				if (flat.length && flat[flat.length - 1].url === this.page.url) {
+					return item;
+				}
+				flat.push(item);
+
+				const childReturn = visit(item.children);
+				if (childReturn) {
+					return childReturn;
+				}
+			}
+		}
+		return visit(nav);
+	});
+
 	eleventyConfig.addPassthroughCopy("assets");
 	eleventyConfig.addPassthroughCopy("node_modules");
-	eleventyConfig.setServerOptions({});
 
 	return {
 		markdownTemplateEngine: "njk",
